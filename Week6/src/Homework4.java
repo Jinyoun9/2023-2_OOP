f43aae79aede9b4ecd20b8ededdbe9a368ed2804
Park Jinyoung <glitt5384@naver.com>
2023-10-14 20:57:24 +0900

    Homework4.java

--- Week6/src/Homework4.java
+++ Week6/src/Homework4.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class Homework4 {
-    /*int gcd(int m, int n){
+    int gcd(int m, int n){
         if (m == 0) return m;
         else if (m % n == 0) return n;
         else{
             m = m % n;
             return gcd(n, m);
         }
-    }*/
-    int gcd(int m, int n){
+    }
+    /*int gcd(int m, int n){
         while(true) {
             if (m == 0) return m;
             else if (m % n == 0) {
@@ -22,13 +22,12 @@ public class Homework4 {
             }
         }
 
-    }
+    }*/
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Homework4 hw = new Homework4();
         int m = sc.nextInt();
         int n = sc.nextInt();
-        int res = hw.gcd(m, n);
-        System.out.printf("%d\n", res);
+        System.out.printf("%d\n", hw.gcd(m, n));
     }
 }
