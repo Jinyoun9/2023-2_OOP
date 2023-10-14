a93b0c365bf75c909361d52981c7963933e77812
Park Jinyoung <glitt5384@naver.com>
2023-10-14 21:12:03 +0900

    Homework4.java

--- Week6/src/Homework4.java
+++ Week6/src/Homework4.java
@@ -28,6 +28,7 @@ public class Homework4 {
         Homework4 hw = new Homework4();
         int m = sc.nextInt();
         int n = sc.nextInt();
-        System.out.printf("%d\n", hw.gcd(m, n));
+        int res = hw.gcd(m, n);
+        System.out.printf("%d\n", res);
     }
 }
